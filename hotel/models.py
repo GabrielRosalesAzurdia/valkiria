0f8b977bae58f23f5b48572cc8af1d181bff6058
Gabriel Rosales <50784668+GabrielRosalesAzurdia@users.noreply.github.com>
2023-10-20 11:30:21 -0600

    agregando getuser

--- hotel/models.py
+++ hotel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Clientes (models.Model):
     edad = models.IntegerField
@@ -17,7 +18,6 @@ class Reserva (models.Model):
     nocheIngreso = models.DateField
     nocheSalida = models.DateField
 
-# add user model (employee)
-# class Factura (models.Model):
-#     reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
-#     empleado = models.ForeignKey(, on_delete=models.CASCADE)
+class Factura (models.Model):
+    reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
